7359708dc2b243bc07b5e073e14467dc3675152e
mil <miles.sandlar@gmail.com>
2013-02-08 19:20:45 -0500

    Add some title text and divider indent/stylizing

--- js/mil_edit.template.js
+++ js/mil_edit.template.js
@@ -1,11 +1,11 @@
 var template_buttons = ''.concat(
   '<div id="buttons">',
 
-  '<a id="undent" title="< or Ctrl-&larr;" onclick="mil_edit.undent()"></a>',
-  '<a id="indent" title="> or Ctrl-&rarr;" onclick="mil_edit.indent()"></a>',
-
-  '<a id="bold" title="Ctrl-B" onclick="mil_edit.bold()"></a>',
-  '<a id="italic" title="Ctrl-I" onclick="mil_edit.italic()"></a>',
+  '<a id="undent" title="Press < or Ctrl-&larr; to Undent" onclick="mil_edit.undent()"></a>',
+  '<a id="indent" title="Press > or Ctrl-&rarr; to Indent" onclick="mil_edit.indent()"></a>',
+  '<span class="separator"></span>',
+  '<a id="bold" title="Press Ctrl-B to **Bold** Text" onclick="mil_edit.bold()"></a>',
+  '<a id="italic" title="Press Ctrl-I to _Italicize_ Text" onclick="mil_edit.italic()"></a>',
 
   '<a id="keys" onclick="mil_edit.keybindings()"><span>Key Controls</span></a>',
   '</div>'
@@ -28,7 +28,7 @@ var template_keybindings = ''.concat(
   '<span class="key">&uarr;</span> and <span class="key">&darr;</span> move between lines<br/>',
   '<span class="key">Shift</span> <span class="key">&uarr;</span> <span class="key">&darr;</span> shifts the line',
   '</div>',
-  '<a id="collapse" onclick="mil_edit.keybindings()">Collapse &rarr;</a>',
+  '<a id="collapse" title="Hide this Help Pane" onclick="mil_edit.keybindings()">Collapse &rarr;</a>',
 
   '</div>'
 );
